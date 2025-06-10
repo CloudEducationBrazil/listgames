58f59b44ba11199632a76b77bc852dc0fa1c38e2
Heleno Cardoso <helenocardosofilho@gmail.com>
2025-06-10 12:29:57 -0300

    Proj Web Java Postgresql vai .. 10062025

--- src/main/java/com/uniruy/listgames/controllers/HealthController.java
+++ src/main/java/com/uniruy/listgames/controllers/HealthController.java
@@ -1,12 +1,14 @@
 package com.uniruy.listgames.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/")
 public class HealthController {
 
-    @GetMapping("/")
+    @GetMapping
     public String root() {
         return "ListGames API em produção!";
     }
