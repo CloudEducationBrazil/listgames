eef46efbff8dad51c2f3f8df1e2b2f6b12238420
Heleno Cardoso <helenocardosofilho@gmail.com>
2025-06-11 16:04:35 -0300

    Proj Web Java Postgresql 11062025

--- src/main/java/com/uniruy/listgames/repositories/GameRepository.java
+++ src/main/java/com/uniruy/listgames/repositories/GameRepository.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-//import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository;
 
 import com.uniruy.listgames.entities.Game;
 import com.uniruy.listgames.projections.GameMinProjection;
 
-//@Repository
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 	@Query(nativeQuery = true, value = """
 			SELECT tb_game.id, tb_game.title, tb_game.game_year AS "year", tb_game.img_url AS imgUrl,
